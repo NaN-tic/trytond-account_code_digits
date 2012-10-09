ed7110a270c7915276f09a51d21ac3383324703a
resteve <resteve@zikzakmedia.com>
2012-10-09 13:17:31 +0200

    Simplify module information with python configuration

--- setup.py
+++ setup.py
@@ -5,8 +5,15 @@
 
 from setuptools import setup
 import re
+import os
+import ConfigParser
 
-info = eval(open('__tryton__.py').read())
+config = ConfigParser.ConfigParser()
+config.readfp(open('tryton.cfg'))
+info = dict(config.items('tryton'))
+for key in ('depends', 'extras_depend', 'xml'):
+    if key in info:
+        info[key] = info[key].strip().splitlines()
 major_version, minor_version, _ = info.get('version', '0.0.1').split('.', 2)
 major_version = int(major_version)
 minor_version = int(minor_version)
@@ -22,10 +29,10 @@ requires.append('trytond >= %s.%s, < %s.%s' %
 
 setup(name='trytond_account_code_digits',
     version=info.get('version', '0.0.1'),
-    description=info.get('description', ''),
-    author=info.get('author', ''),
-    author_email=info.get('email', ''),
-    url=info.get('website', ''),
+    description='Tryton module to expand account codes',
+    author='NaN·tic',
+    author_email='info@NaN-tic.com',
+    url='http://www.nan-tic.com',
     download_url="https://bitbucket.org/albertnan/account_code_digits",
     package_dir={'trytond.modules.account_code_digits': '.'},
     packages=[
@@ -34,7 +41,7 @@ setup(name='trytond_account_code_digits',
     ],
     package_data={
         'trytond.modules.account_code_digits': info.get('xml', []) \
-                + info.get('translation', []),
+            + ['tryton.cfg', 'locale/*.po'],
     },
     classifiers=[
         'Development Status :: 5 - Production/Stable',
