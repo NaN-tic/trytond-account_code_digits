764c5c706486c0b00cb5bab8cd3731a9998c9164
resteve <resteve@zikzakmedia.com>
2013-04-23 14:09:55 +0200

    Revert change rev 16 and lost to merge heads

--- configuration.py
+++ configuration.py
@@ -8,9 +8,11 @@ from trytond.pool import PoolMeta
 __all__ = ['Configuration']
 __metaclass__ = PoolMeta
 
+
 class Configuration:
     __name__ = 'account.configuration'
 
-    default_account_code_digits = fields.Numeric('Account Code Digits', digits=(16, 0),
-            help='Number of digits to be used for all non-view accounts.')
+    default_account_code_digits = fields.Property(
+        fields.Numeric('Account Code Digits', digits=(16, 0),
+            help='Number of digits to be used for all non-view accounts.'))
 
