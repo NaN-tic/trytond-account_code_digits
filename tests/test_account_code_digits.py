de9e91a658539b92e7844eeeb53a655b58b9b392
Sergi Almacellas Abellana <sergi@koolpi.com>
2016-04-11 10:34:47 +0200

    Update tests

--- tests/test_account_code_digits.py
+++ tests/test_account_code_digits.py
@@ -1,57 +1,55 @@
 # This file is part of the account_code_digits module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-import doctest
 import unittest
+from trytond.pool import Pool
 import trytond.tests.test_tryton
 from trytond.exceptions import UserError
-from trytond.tests.test_tryton import ModuleTestCase
-from trytond.tests.test_tryton import POOL, DB_NAME, USER, CONTEXT
-from trytond.transaction import Transaction
+from trytond.tests.test_tryton import ModuleTestCase, with_transaction
+
+from trytond.modules.company.tests import create_company, set_company
+from trytond.modules.account.tests import create_chart
 
 
 class AccountCodeDigitsTestCase(ModuleTestCase):
     'Test Account Code Digits module'
     module = 'account_code_digits'
 
-    def setUp(self):
-        super(AccountCodeDigitsTestCase, self).setUp()
-        self.account = POOL.get('account.account')
-        self.config = POOL.get('account.configuration')
-
+    @with_transaction()
     def test0010_force_digits(self):
         'Test force digits'
-        with Transaction().start(DB_NAME, USER, context=CONTEXT):
-            config = self.config.get_singleton() or self.config()
+        pool = Pool()
+        Account = pool.get('account.account')
+        Config = pool.get('account.configuration')
+        company = create_company()
+        with set_company(company):
+            create_chart(company, tax=False)
+            config = Config.get_singleton() or Config()
             config.default_account_code_digits = 6
             config.force_digits = True
             config.save()
 
-            view, = self.account.search([
+            view, = Account.search([
                     ('kind', '=', 'view'),
                     ], limit=1)
-            non_view,  = self.account.search([
+            non_view, = Account.search([
                     ('kind', '!=', 'view'),
                     ], limit=1)
 
-            self.assertRaises(UserError, self.account.write, [non_view],
+            self.assertRaises(UserError, Account.write, [non_view],
                 {'code': '000'})
-            self.account.write([view], {'code': '0'})
-            self.account.write([non_view], {'code': '000000'})
+            Account.write([view], {'code': '0'})
+            Account.write([non_view], {'code': '000000'})
             self.assertEqual(view.code, '0')
             self.assertEqual(non_view.code, '000000')
             config.force_digits = False
             config.save()
-            self.account.write([non_view], {'code': '0000'})
+            Account.write([non_view], {'code': '0000'})
             self.assertEqual(non_view.code, '0000')
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
-    from trytond.modules.account.tests import test_account
-    for test in test_account.suite():
-        if test not in suite and not isinstance(test, doctest.DocTestCase):
-            suite.addTest(test)
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         AccountCodeDigitsTestCase))
     return suite
